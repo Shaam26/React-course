ae14a9e742709cb3835d283290ad55d322bbb8b8
Shaam26 <shameembanu561@gmail.com>
2025-09-09 21:14:31 +0530

    Added files

---
+++ 18-22 Topics/learning-fragment/src/App.jsx
@@ -0,0 +1,38 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FoodItems from "./components/FoodItems";
+import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
+
+function App() {
+  //let foodItems = [];
+  let foodItems = ["Green vegetable", "Rice", "Water", "Fruits", "Daal"];
+  let textToShow = "Food Item Entered by user";
+
+  const handleOnChange = (event) => {
+    console.log(event.target.value);
+    textToShow = event.target.value;
+  };
+
+  return (
+    <>
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <FoodInput handleOnChange={handleOnChange}></FoodInput>
+        <p>{textToShow}</p>
+        <FoodItems items={foodItems}></FoodItems>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+      </Container>
+
+      {/*<Container>
+        <p>
+          Above is the list of healthy foods that are good for your health and
+          well being
+        </p>
+      </Container>*/}
+    </>
+  );
+}
+
+export default App;
---
+++ 18-22 Topics/learning-fragment/src/components/Items.jsx
@@ -0,0 +1,21 @@
+import styles from "./Items.module.css";
+
+const Items = ({ foodItem }) => {
+  const handleBuyButtonClicked = (event) => {
+    console.log(event);
+    console.log(`${foodItem} being bought`);
+  };
+
+  return (
+    <li className={`${styles["kg-item"]} list-group-item`}>
+      <span className={styles["kg-span"]}>{foodItem}</span>
+      <button
+        className={`${styles.button}  btn btn-info`}
+        onClick={(event) => handleBuyButtonClicked(event)}
+      >
+        Buy
+      </button>
+    </li>
+  );
+};
+export default Items;
